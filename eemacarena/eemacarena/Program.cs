d88c0ae2ad93ebf985395862a4ca688ce73e3a51
Oubha <P1nU21kas>
2019-09-18 14:33:34 +0300

    done

---
+++ eemacarena/eemacarena/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace eemacarena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("syötä luku");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            if (userNumber > 0)
+            {
+                Console.WriteLine("numero on positiivinen");
+            }
+            else if (userNumber == 0)
+                Console.WriteLine("numero on 0");
+             
+            else
+            {
+                Console.WriteLine("numero on negatiivinen");
+            }
+       
+            
+
+          
+        }
+    }
+}
