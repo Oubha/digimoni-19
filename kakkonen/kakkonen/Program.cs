3f6b8e8c559044c1c9fe014996489cbd2c481573
Oubha <P1nU21kas>
2019-09-18 14:53:14 +0300

    done

---
+++ kakkonen/kakkonen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace kakkonen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+            Console.WriteLine("syötä luku");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber % 2 == 0)
+            {
+                Console.WriteLine("luku on parillinen");
+            }
+            else
+            {
+                Console.WriteLine("luku on pariton");
+            }
+        }
+    }
+}
