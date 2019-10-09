8048a44bc197cdbc4c5934b943527b8ab1ccd6c3
Oubha <P1nU21kas>
2019-10-09 17:21:58 +0300

    1 done

---
+++ dowhilefor/dowhilefor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace dowhilefor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int i = 1;
+            int vastaus = 1;
+
+            Console.WriteLine("syötä luku, jonka kertoma lasketaan");
+            Console.Write("syötä luku: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n >= 1)
+            {
+                while (i <= n)
+                {
+                    vastaus = (vastaus * i);
+                    i++;
+                }
+                Console.WriteLine("vastaus on: " + vastaus);
+            }
+            else
+            {
+                Console.WriteLine("luku on liian pieni");
+            }
+        }
+    }
+}
