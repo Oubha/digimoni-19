b3cd6444e3b39790750bdb2977225e3129c79d00
Oubha <P1nU21kas>
2019-10-18 14:52:11 +0300

    v2 done

---
+++ if1.2/if1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace if1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("syötä luku");
+           int userNumber = int.Parse(Console.ReadLine());
+            if (userNumber %2 == 0 )
+            {
+                Console.WriteLine("luku on parillinen");
+            }
+            else
+            {
+                Console.WriteLine("luku on pariton");
+            }
+        }
+    }
+}
