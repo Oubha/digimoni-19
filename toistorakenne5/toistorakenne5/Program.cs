dbab230c254850cf82ad5ecc93d1ef5c4737f748
Oubha <P1nU21kas>
2019-10-31 13:04:20 +0200

    v1done

---
+++ toistorakenne5/toistorakenne5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace toistorakenne5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("ohjelma tulostaa vakioveikkauksen rivin");
+
+            for (int i = 1; i < 14; i++)
+            {
+                double tulos = rnd.NextDouble();
+
+                if (tulos <= 0.4)
+                {
+                    Console.WriteLine($"{i}. kotivoitto");
+                }
+                else if (tulos <= 0.6 && tulos > 0.4)
+                {
+                    Console.WriteLine($"{i}. tasapeli");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}. vierasvoitto");
+                }
+            }
+        }
+    }
+}
