c4b1ee7120419f6e4a8dd8bb45f9e1c508681c94
Oubha <P1nU21kas>
2019-10-31 14:35:39 +0200

    done

---
+++ itsearviointitehtävät/arviointi1/arviointi1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace arviointi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int number = 5;
+            //ohjelma tulostaa viisi kertaa käyttäjän syöttämän sanan tai lauseen
+            Console.WriteLine("syötä haluamasi sana tai lause");
+            text = Console.ReadLine();
+             for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"{text}");
+            }
+        }
+    }
+}
