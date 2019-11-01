afcad47b6c3379869c66992c44e73c2b0b211d11
Oubha <P1nU21kas>
2019-11-01 15:08:58 +0200

    jee

---
+++ funktiot/funktio2/funktio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace funktio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma palauttaa kahdesta annetusta luvusta pienemmän");
+            Console.WriteLine("syötä kaksi lukua");
+            int userNumber1 = int.Parse(Console.ReadLine());
+            int userNumber2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pienempi luku on: " +minimi(userNumber1, userNumber2));
+
+        }
+        public static int minimi(int userNumber1, int userNumber2)
+        {
+
+            return userNumber1 < userNumber2 ? userNumber1 : userNumber2;
+
+        }
+    }
+}
