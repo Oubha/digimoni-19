3194575e9eec3a4931ff3552cde8ab12da3e96bb
Oubha <P1nU21kas>
2019-11-01 15:56:23 +0200

    done

---
+++ itsearviointitehtävät/arviointi2/arviointi2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace arviointi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            //ohjelma tulostaa syötetyn lauseen tai sanan niin monta kertaa, kuin sanassa tai lauseessa on kirjaimia
+            Console.WriteLine("syötä haluamasi sana tai lause");
+            text = Console.ReadLine();
+
+            int x = text.Length;
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
