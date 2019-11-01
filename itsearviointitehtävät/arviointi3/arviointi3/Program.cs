7f70d1eb49ad0782dcb426bef885d94a6431c852
Oubha <P1nU21kas>
2019-11-01 16:23:40 +0200

    valmis

---
+++ itsearviointitehtävät/arviointi3/arviointi3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace arviointi3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma laskee annettuja lukuja yhteen, kunnes sille syötetään -1");
+            int lukuja=0;
+
+            for (int i = 0; i != -1;)
+            {
+                Console.WriteLine("Syötä luku: ");
+                i = int.Parse(Console.ReadLine());
+
+                if (i != -1)
+                {
+                    lukuja += i;
+                }
+            }
+
+            Console.WriteLine("Lukujen summa on: " + lukuja);
+        }
+    }
+}
