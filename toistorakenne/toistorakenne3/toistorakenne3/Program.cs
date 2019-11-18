e3a68550d80882e0f744d0a10061a75984cad899
Oubha <P1nU21kas>
2019-11-18 15:06:46 +0200

    re-filed

---
+++ toistorakenne/toistorakenne3/toistorakenne3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace toistorakenne3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n = 1;
+            Console.WriteLine("ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0-50");
+            Random rnd = new Random();
+
+            while (n <= 5)
+            {
+                Console.Write("rivi " + n + ": ");
+
+                while (i < 4)
+                {
+                    Console.WriteLine(rnd.Next(50) + ", ");
+                    i++;
+                    
+                    if (i == 4)
+                    {
+                        Console.WriteLine(rnd.Next(50) + ", ");
+                    }
+                }
+                i = 0;
+                n++;
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
