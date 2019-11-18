3eef075688b75e3defcd478fda581b2af75f4aa3
Oubha <P1nU21kas>
2019-11-18 15:59:24 +0200

    re-filed

---
+++ do-while-for/dowhilefor/dowhilefor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace dowhilefor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int i = 1;
+            int vastaus = 1;
+
+            Console.WriteLine("syötä luku, jonka kertoma lasketaan");
+            n = int.Parse(Console.ReadLine());
+
+            if (n >= 1)
+            {
+                while (i <= n)
+                {
+                    vastaus = (vastaus * i);
+                    i++;
+                }
+                Console.WriteLine("vastaus on: " + vastaus);
+            }
+            else
+            {
+                Console.WriteLine("luku on liian pieni");
+            }
+        }
+    }
+}
---
+++ do-while-for/dwf2neg/dwf2neg/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace dwf2neg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("syötä luku, johon asti lisätään tai vähennetään");
+            int i = int.Parse(Console.ReadLine());
+            int n = 0;
+            int p = 0;
+
+            if (i > 0)
+            {
+                while (n <= i)
+                {
+                    p = (p + n);
+                    n++;
+                }
+                Console.WriteLine($"tulos on: {p}");
+            }
+            else
+            {
+                if (i < 0)
+                {
+                    while (n >= i)
+                    {
+                        p = (p + n);
+                        n--;
+                    }
+                    Console.WriteLine($"tulos on: {p}");
+                }
+            }
+        }
+    }
+}
