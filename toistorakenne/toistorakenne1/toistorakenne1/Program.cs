ba21149da5d1d333e0d04adf344a8bc8bb8b1a20
Oubha <P1nU21kas>
2019-11-18 15:05:32 +0200

    re-filed

---
+++ toistorakenne/toistorakenne1/toistorakenne1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace toistorakenne1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma tulostaa kokonaisluvut ja neliöjuuret luvuille 1-10");
+            Console.WriteLine("luku |    neliöjuuri");
+            double i = 0;
+
+            while (i <= 10)
+            {
+                double nelio = Math.Sqrt(i);
+                Console.WriteLine($"{i}    |    {nelio}");
+                i++;
+            }
+            return;
+        }
+    }
+}
