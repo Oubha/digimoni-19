adec1ab60e5fa7cd1a7d6a293c9ac3e8fd90902b
Oubha <P1nU21kas>
2019-11-18 15:58:14 +0200

    re-filed

---
+++ if-harjoitukset/ifkolme/ifkolme/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ifkolme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+            Console.WriteLine("syötä luku");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber % 2 == 0)
+            {
+                Console.WriteLine("luku on parillinen");
+            }
+            else
+            {
+                Console.WriteLine("luku on pariton");
+            }
+     
+            if (userNumber > 0)
+            {
+                Console.WriteLine("luku on positiivinen");
+            }
+            else if (userNumber == 0)
+                Console.WriteLine("numero on 0");
+            else
+            {
+                Console.WriteLine("luku on negatiivinen");
+            }
+        }
+    }
+}
