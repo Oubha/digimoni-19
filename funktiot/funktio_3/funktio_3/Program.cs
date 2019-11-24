e7913cd0f1ceb87339696e94f8c92cd4bf59d62f
Oubha <P1nU21kas>
2019-11-24 16:12:49 +0200

    done v1

---
+++ funktiot/funktio_3/funktio_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace funktio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int retNumber = numberFromRange(1, 50);
+
+            Console.WriteLine($"syötetty luku {retNumber} on annetulta väliltä!");
+
+        }
+        static int numberFromRange(int lowerBound, int upperBound)
+        {
+            bool isNumber;
+            int input = 0;
+
+            do
+            {
+                Console.WriteLine($"syötä luku väliltä {lowerBound} ja {upperBound}: ");
+                isNumber = int.TryParse(Console.ReadLine(), out input);
+
+                if (input <= upperBound && input >= lowerBound)
+                {
+                    break;
+                }
+                else
+                {
+                    isNumber = false;
+                    Console.WriteLine($"annettu luku ei ole luku tai väliltä {lowerBound} ja {upperBound}. Yritä uudestaan");
+                }
+            }
+            while (isNumber == false);
+            return input;
+        }
+    }
+}
