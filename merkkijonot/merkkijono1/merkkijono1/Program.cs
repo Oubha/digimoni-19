0ed1cddef18ae4650c467e9c379443eb2703f12f
Oubha <P1nU21kas>
2019-11-26 11:18:03 +0200

    v1 done

---
+++ merkkijonot/merkkijono1/merkkijono1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace merkkijono1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("merkkijonon käsittelyä");
+            string message = "huutista";
+
+            //tulostetaan muuttujien merkit 'message' kohdasta yksitellen näytölle
+            Console.WriteLine($"{message} on {message.Length} merkkiä pitkä.");
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine(message[i]);
+            }
+        }
+    }
+}
