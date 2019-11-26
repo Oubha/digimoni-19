9b2ff8c0b4c346deb53fbe2e27c92926529c3a78
Oubha <P1nU21kas>
2019-11-26 10:42:48 +0200

    errors

--- funktiot/funktio_4/funktio_4/Program.cs
+++ funktiot/funktio_4/funktio_4/Program.cs
@@ -7,8 +7,40 @@ namespace funktio_4
         static void Main(string[] args)
         {
             Console.WriteLine("ohjelma pyytää käyttäjältä 10 positiivista kokonaislukua ja palauttaa niistä isoimman");
-            string resultMessage = MaxNumberCalculator();
+            string resultMessage = MaxNumberCalculator(ref maxNumber, ref SequenceOfNumber);
             Console.WriteLine($"syötit ohjelmalle seuraavat luvut :\n {resultMessage}");
+            Console.WriteLine($"isoin luku oli {maxNumber} ja se oli {sequenceOfNumber}. luku");
+        }
+        static string MaxNumberCalculator(ref int maxNumber, ref int sequenceOfNumber)
+        {
+            //maxNumber=0;
+            //sequenceOfNumber=0;
+            string message = string.Empty;
+
+            Console.WriteLine("syötä kymmenen kokonaislukua");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}");
+                string userInput = Console.ReadLine();
+                bool isNumber = int.TryParse(userInput, out int userValue);
+
+                if (isNumber)
+                {
+                    //generoidaan palautettava viesti annetuista luvuista
+                    message = $"{message} {userValue}";
+                    if (userValue > maxNumber)
+                    {
+                        maxNumber = userValue;
+                        sequenceOfNumber = i + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("väärä luku, kokeile uudestaan");
+                        i--;
+                    }
+                }
+                return message;
+            }
         }
     }
 }
