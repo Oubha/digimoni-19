4bfaa901e5dbd9ecf33393e4e997f1f9c9f60649
Oubha <P1nU21kas>
2019-12-01 12:16:28 +0200

    done

---
+++ merkkijonot/merkkijono3/merkkijono3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace merkkijono3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma laskee syötetystä sanasta tai lauseesta k kirjainten määrän");
+            string message = Console.ReadLine();
+            int count = 0;
+            char charToCount = 'k';
+            foreach (char k in message)
+            {
+                if (k == charToCount)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"{message}-syötteessä on {count} k kirjainta");
+        }
+    }
+}
