14bdb9a0803482ae9d03ac016d5ee9b4fb8017ea
Oubha <P1nU21kas>
2020-01-14 13:33:54 +0200

    merkkijono4 created

---
+++ merkkijonot/merkkijono4/merkkijono4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace merkkijono4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("syötä sana tai lause, ohjelma tarkistaa, onko se palintromi.");
+
+        }
+    }
+}
